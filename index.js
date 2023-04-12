8b0dff250981c3a25c6aec600d488b6fdfc5bf79
Rohit Ingole <ingolerohit2017@gmail.com>
2023-04-12 11:46:13 +0530

    changes small a to capital A

---
+++ index.js
@@ -0,0 +1,11 @@
+//Given a charator in lower case print it in UPPER CASE -----
+
+var char = "g";
+var lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+for (var i=0; i<=lower.length; i++){
+    if(char== lower[i]){
+        console.log(upper[i]);
+        break;
+    }
+}
